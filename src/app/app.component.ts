a1d6dd2cecc4c8256c0ec2fa37393699303f6f80
sofiane <monzinou1@gmail.com>
2019-11-10 11:08:03 +0100

    he first version of the app ,it's containe just a login page with a fake backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { User } from './_models';
+import { Router } from '@angular/router';
+import { AuthenticationService } from './_services';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'IsticharaDash';
+  currentUser:User;
+  constructor(
+    private router:Router,
+    private authenticationService:AuthenticationService
+  )
+  {
+    this.authenticationService.currentUser.subscribe(
+      x=>this.currentUser=x
+    );
+  }
+  logout(){
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+ import {fakeBackendProvider} from './_helpers';
+ import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {provide:HTTP_INTERCEPTORS,useClass:JwtInterceptor,multi:true},
+    {provide:HTTP_INTERCEPTORS,useClass:ErrorInterceptor,multi:true}
+  ,fakeBackendProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
